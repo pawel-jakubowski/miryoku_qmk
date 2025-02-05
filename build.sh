27b784d5ed492fd93cf24cf5bf52e356901074b3
Pawel Jakubowski <pawel.jakubowski@nokia.com>
2025-02-05 10:02:31 +0100

    add build script and update layout

---
+++ build.sh
@@ -0,0 +1,3 @@
+qmk compile -c -kb crkbd -km manna-harbour_miryoku -e MIRYOKU_CLIPBOARD=WIN
+cp crkbd_rev1_manna-harbour_miryoku.hex /mnt/c/Users/pjakubow/crkbd_rev1_manna-harbour_miryoku1.hex
+echo "Copied to /mnt/c/Users/pjakubow/crkbd_rev1_manna-harbour_miryoku1.hex"
